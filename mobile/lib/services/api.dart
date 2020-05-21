f198f8c0ce78fd5abf16b69842c7d03e2e072f3a
Caique <caiiquexd@gmail.com>
2020-05-21 20:57:03 -0300

    deploy

--- mobile/lib/services/api.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dio/dio.dart';
-
-class Server {
-  static Dio request() => Dio(BaseOptions(
-        baseUrl: "http://152.67.61.13:3333",
-        connectTimeout: 5000,
-        receiveTimeout: 3000,
-      ));
-}
